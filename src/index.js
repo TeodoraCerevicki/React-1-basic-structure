236624c543868cbb1b1ad28311bbe3d71bca795d
Teodora <teodora.cerevicki@gmail.com>
2023-07-04 11:20:56 +0200

    basic-structure

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const el = document.getElementById('root');
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+const root = ReactDOM.createRoot(el);
+
+function AppIF() {
+    let message = 'Bye there!'
+
+    if (Math.random() > 0.5) {
+        message = 'Hi there!'
+    }
+    return <h1>{message}</h1>;
+}
+
+function AppDate() {
+    return <h1>{new Date().toLocaleTimeString()}</h1>;
+}
+
+function AppConst() {
+    const name = 'Tea';
+    const age = 32;
+
+    return <h1>HOLA! my name is {name}, and im {age}</h1>;
+}
+
+function AppInput() {
+    const type = 'number';
+    const minValues = 5
+    return <input type={type} min={minValues} max={10} />;
+}
+
+root.render(<AppIF />);
+root.render(<AppDate />);
+root.render(<AppConst />);
+root.render(<AppInput />);
+
